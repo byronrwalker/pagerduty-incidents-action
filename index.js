c13077d21852c6f268381a50dca9144e630e5eb6
Grace Park <gracepark@github.com>
2024-09-09 08:47:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,13 @@ async function run() {
 async function fetchIncidents(scheduleId, startTime, endTime, headers) {
   const pd = api({token: `${pagerdutyToken}`});
   pd.get('/incidents')
-    .then({data, resource, next} => console.log(data, resource, next))
-    .catch(console.error);
+  .then({resource, data, response, next} => {
+    console.log(resource); // Contains the data of resource request. In this example the 'incidents' data.
+    console.log(data);     // The raw data returned from the API, also contains pagination information.
+    console.log(response); // The response object returned from the cross-fetch
+    console.log(next);     // A convenience function to help with pagination
+  })
+  .catch(console.error);
 }
 
 run();
