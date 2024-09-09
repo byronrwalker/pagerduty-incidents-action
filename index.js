975c483388d6d34912c9a3a142ccdaf2ec4e66ff
Grace Park <gracepark@github.com>
2024-09-09 11:18:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ async function run() {
     const startTime = core.getInput('start_time');
     const endTime = core.getInput('end_time');
     const pagerdutyToken = core.getInput('pagerduty_token');
-
+    const teamId = core.getInput('team_id');
+    
     // Set the headers
     const headers = {
       'Authorization': `Token token=${pagerdutyToken}`,
@@ -16,26 +17,31 @@ async function run() {
     };
 
     // Get incidents from PagerDuty
-    const incidents = await fetchIncidents(scheduleId, startTime, endTime, headers);
+    const incidents = await fetchIncidents(pagerdutyToken, teamId, startTime, endTime);
     
-    // if (incidents.length === 0) {
-    //   core.info('No incidents found for the given schedule and time range.');
-    // } else {
-    //   core.info(`Found ${incidents.length} incidents:`);
-    //   incidents.forEach(incident => {
-    //     core.info(`- Incident ID: ${incident.id}, Description: ${incident.description}`);
-    //   });
-    // }
+    if (incidents.length === 0) {
+      core.info('No incidents found for the given schedule and time range.');
+    } else {
+      core.info(`Found ${incidents.length} incidents:`);
+      incidents.forEach(incident => {
+        core.info(`- Incident ID: ${incident.id}, Description: ${incident.description}`);
+      });
+    }
 
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function fetchIncidents(scheduleId, startTime, endTime, headers) {
+async function fetchIncidents(pagerdutyToken, team_id, startTime, endTime) {
   const pd = api({token: `${pagerdutyToken}`});
-  pd.get('/incidents')
-    .then({data, resource, next} => console.log(data, resource, next))
+  pd.get('/incidents', {
+    data: {
+      "team_ids[]": team_id,
+      "since": startTime.toISOString(),
+      "until": endTime.toISOString(),
+    }
+  }).then(({data, resource, next}) => console.log(data, resource, next))
     .catch(console.error);
 }
 
